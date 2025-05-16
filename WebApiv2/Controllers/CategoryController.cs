7e2c29707019c61581b2cd46ba7f9869758d579b
Mauri Ribotta <mauriribotta2@gmail.com>
2025-05-15 23:18:17 -0300

    Update CategoryController.cs

--- WebApiv2/Controllers/CategoryController.cs
+++ WebApiv2/Controllers/CategoryController.cs
@@ -1,12 +1,66 @@
+using Application.Categories.Commands.CreateCategory;
+using Application.Categories.Commands.DeleteCategory;
+using Application.Categories.Commands.UpdateCategory;
+using Application.Categories.Queries.GetAllCategories;
+using Application.Categories.Queries.GetCategoryById;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApiv2.Controllers;
 
-public class CategoryController : Controller
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriesController : ControllerBase
 {
-    // GET
-    public IActionResult Index()
+    private readonly IMediator _mediator;
+
+    public CategoriesController(IMediator mediator)
     {
-        return View();
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _mediator.Send(new GetAllCategoriesQuery());
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var category = await _mediator.Send(new GetCategoryByIdQuery(id));
+        return category == null ? NotFound() : Ok(category);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateCategoryCommand command)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var result = await _mediator.Send(command);
+        return CreatedAtAction(nameof(GetById), new { id = result.CategoryId }, result);
+
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(Guid id, [FromBody] UpdateCategoryCommand command)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        if (id != command.CategoryId)
+            return BadRequest("El ID de la ruta no coincide con el ID del cuerpo de la solicitud");
+
+        var result = await _mediator.Send(command);
+        return result ? Ok() : NotFound();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var result = await _mediator.Send(new DeleteCategoryCommand(id));
+        return result ? NoContent() : NotFound();
     }
 }
